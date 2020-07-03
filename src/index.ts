6ec7587276d7af07c38151b075f69eb86932ed25
ipaullly <ipaully22@gmail.com>
2020-07-03 17:44:35 +0300

    add controllers and error handler middlewares

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { itemsRouter } from './items/items.router'
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/notFound.middleware";
 
 dotenv.config();
 /**
@@ -23,6 +26,10 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use('/items', itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 /**
  * Server Activation
  */
